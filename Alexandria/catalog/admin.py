4b37f7dd1561c0a8b27529cf0ef966434f3dbc61
Volkmar Petschnig <Volkmar.Petschnig@verbund.com>
2023-12-27 12:45:02 +0100

    Add AuthorAdmin to admin.py

--- Alexandria/catalog/admin.py
+++ Alexandria/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Book, Author
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'birth_year')
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author_last_name')
+    list_filter = ('author', )
+
+    def author_last_name(self, obj):
+        return obj.author.last_name
