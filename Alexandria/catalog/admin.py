ee9c360a567692453ccc18c25577c48159774063
Volkmar Petschnig <Volkmar.Petschnig@verbund.com>
2023-12-27 14:16:06 +0100

    Add custom list_display

--- Alexandria/catalog/admin.py
+++ Alexandria/catalog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from catalog.models import Book, Author
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'birth_year')
+    list_display = ('id', 'last_name', 'first_name', 'show_books')
 
+    def show_books(selfself, obj):
+        count = Book.objects.filter(author=obj).count()
+        url = reverse("admin:catalog_book_changelist") + f'?author__id={obj.id}'
+        plural = 's' if count != 1 else ''
 
+        return format_html('<a href="{}">{} Book{}</a>', url, count, plural)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
